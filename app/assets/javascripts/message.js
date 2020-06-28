67ae21d2c1869c22fd267d7eb4b057a39288005a
ik425524 <yipingxiaoxi286@gmail.com>
2020-06-28 15:52:46 +0900

    test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,39 @@
 $(function () {
+  function buildHTML(message) {
+    if (message.image) {
+      var html = `<div class="chat-main__message-list__group">
+           <div class="chat-main__message-list__group__name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list__group__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message-list__message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>
+         <img src=${message.image} >
+        </div> `;
+      return html;
+    } else {
+      var html = `<div class="chat-main__message-list__group">
+           <div class="chat-main__message-list__group__name">
+             ${message.user_name}
+           </div>
+           <div class="chat-main__message-list__group__date">
+             ${message.created_at}
+           </div>
+         </div>
+         <div class="chat-main__message-list__message">
+           <p class="lower-message__content">
+             ${message.content}
+           </p>
+         </div>`;
+      return html;
+    }
+  }
   $("#new_message").on("submit", function (e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +45,19 @@ $(function () {
       dataType: "json",
       processData: false,
       contentType: false,
-    });
+    })
+      .done(function (data) {
+        var html = buildHTML(data);
+        $(".chat-main__message-list").append(html);
+        $(".chat-main__message-list").animate({
+          scrollTop: $(".chat-main__message-list")[0].scrollHeight,
+        });
+        $(".submit-btn").prop("disabled", false);
+        $("form")[0].reset();
+      })
+      .fail(function () {
+        $(".submit-btn").prop("disabled", false);
+        alert("メッセージ送信に失敗しました");
+      });
   });
 });
